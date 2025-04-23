d11f0cde135d8ecbd4cc70a9417f962e641edb99
rshalnov <shalnovru@yandex.ru>
2025-04-23 11:33:22 +0300

    Commit in hw-18-webflux

---
+++ HW18-webflux/client-service/build.gradle.kts
@@ -0,0 +1,16 @@
+group = "ru.otushw"
+version = "unspecified"
+
+dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-core")
+
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.google.code.findbugs:jsr305")
+
+    implementation("org.webjars:webjars-locator-core")
+    implementation("org.webjars:sockjs-client")
+    implementation("org.webjars:stomp-websocket")
+    implementation("org.webjars:bootstrap")
+}
+
